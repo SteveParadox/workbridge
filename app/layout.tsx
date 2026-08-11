7a68de49ee905a7884f5ee38141234bb19eebe2a
SteveParadox <fordstphn@gmail.com>
2026-08-11 12:26:10 +0100

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,16 @@ export const metadata: Metadata = {
   robots: {
     index: true,
     follow: true
-  }
+  },
+  icons: {
+    icon: [
+      { url: "/favicon_io/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon_io/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon_io/favicon.ico", rel: "shortcut icon" }
+    ],
+    apple: "/favicon_io/apple-touch-icon.png"
+  },
+  manifest: "/favicon_io/site.webmanifest"
 };
 
 export default function RootLayout({
